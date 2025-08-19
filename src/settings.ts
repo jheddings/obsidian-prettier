216973d5287fbaae21bfb24ded13ebd148942822
jheddings <jason@heddings.com>
2025-08-19 10:02:49 -0600

    Initial structure for setting groups.

--- src/settings.ts
+++ src/settings.ts
@@ -4,12 +4,109 @@ import { PluginSettingTab, App, Setting } from "obsidian";
 import { LogLevel } from "./logger";
 import PrettierPlugin from "./main";
 
+/**
+ * Base class for settings tab pages.
+ */
+abstract class SettingsTabPage {
+    public isActive: boolean = false;
+
+    protected _plugin: PrettierPlugin;
+    protected _name: string;
+
+    /**
+     * Creates a new SettingsTabPage instance.
+     */
+    constructor(plugin: PrettierPlugin, name: string) {
+        this._plugin = plugin;
+        this._name = name;
+    }
+
+    /**
+     * Gets the tab page ID.
+     * @returns The tab page ID string.
+     */
+    get id(): string {
+        return this._name.toLowerCase().replace(/\s+/g, "-");
+    }
+
+    /**
+     * Gets the tab page name.
+     * @returns The tab page name string.
+     */
+    get name(): string {
+        return this._name;
+    }
+
+    abstract display(containerEl: HTMLElement): void;
+}
+
+/**
+ * Settings page for general options.
+ */
+class GeneralSettings extends SettingsTabPage {
+    /**
+     * Creates a new GeneralSettings instance.
+     */
+    constructor(plugin: PrettierPlugin) {
+        super(plugin, "General");
+    }
+
+    /**
+     * Displays the general settings UI.
+     */
+    display(containerEl: HTMLElement): void {
+        new Setting(containerEl)
+            .setName("Auto format")
+            .setDesc("Automatically keep files formatted when changed")
+            .addToggle((toggle) => {
+                toggle.setValue(this._plugin.settings.autoFormat);
+                toggle.onChange(async (value) => {
+                    this._plugin.settings.autoFormat = value;
+                    await this._plugin.saveSettings();
+                });
+            });
+    }
+}
+
+/**
+ * Settings page for advanced options.
+ */
+class AdvancedSettings extends SettingsTabPage {
+    /**
+     * Creates a new AdvancedSettings instance.
+     */
+    constructor(plugin: PrettierPlugin) {
+        super(plugin, "Advanced");
+    }
+
+    /**
+     * Displays the advanced settings UI.
+     */
+    display(containerEl: HTMLElement): void {
+        new Setting(containerEl)
+            .setName("Log level")
+            .setDesc("Set the logging level for debug output")
+            .addDropdown((dropdown) => {
+                dropdown.addOption(LogLevel.ERROR.toString(), "Error");
+                dropdown.addOption(LogLevel.WARN.toString(), "Warning");
+                dropdown.addOption(LogLevel.INFO.toString(), "Info");
+                dropdown.addOption(LogLevel.DEBUG.toString(), "Debug");
+                dropdown.setValue(this._plugin.settings.logLevel.toString());
+                dropdown.onChange(async (value) => {
+                    this._plugin.settings.logLevel = parseInt(value) as LogLevel;
+                    await this._plugin.saveSettings();
+                });
+            });
+    }
+}
+
 export class PrettierSettingsTab extends PluginSettingTab {
-    private plugin: PrettierPlugin;
+    private tabs: SettingsTabPage[];
 
     constructor(app: App, plugin: PrettierPlugin) {
         super(app, plugin);
-        this.plugin = plugin;
+
+        this.tabs = [new GeneralSettings(plugin), new AdvancedSettings(plugin)];
     }
 
     /**
@@ -19,30 +116,51 @@ export class PrettierSettingsTab extends PluginSettingTab {
         const { containerEl } = this;
         containerEl.empty();
 
-        new Setting(containerEl)
-            .setName("Auto format")
-            .setDesc("Automatically keep files formatted when changed")
-            .addToggle((toggle) => {
-                toggle.setValue(this.plugin.settings.autoFormat);
-                toggle.onChange(async (value) => {
-                    this.plugin.settings.autoFormat = value;
-                    await this.plugin.saveSettings();
-                });
+        const tabContainer = containerEl.createEl("div", {
+            cls: "prettier-settings-tab-container",
+        });
+
+        const tabContentDiv = containerEl.createEl("div");
+
+        this.tabs.forEach((tab) => {
+            const tabEl = tabContainer.createEl("button", {
+                text: tab.name,
+                cls: "prettier-settings-tab-button",
             });
 
-        new Setting(containerEl)
-            .setName("Log level")
-            .setDesc("Set the logging level for debug output")
-            .addDropdown((dropdown) => {
-                dropdown.addOption(LogLevel.ERROR.toString(), "Error");
-                dropdown.addOption(LogLevel.WARN.toString(), "Warning");
-                dropdown.addOption(LogLevel.INFO.toString(), "Info");
-                dropdown.addOption(LogLevel.DEBUG.toString(), "Debug");
-                dropdown.setValue(this.plugin.settings.logLevel.toString());
-                dropdown.onChange(async (value) => {
-                    this.plugin.settings.logLevel = parseInt(value) as LogLevel;
-                    await this.plugin.saveSettings();
+            tabEl.addEventListener("click", () => {
+                tabContentDiv.empty();
+
+                this.tabs.forEach((jtab) => {
+                    jtab.isActive = jtab.id === tab.id;
                 });
+
+                this.updateTabButtonStyles(tabContainer);
+
+                tab.display(tabContentDiv);
             });
+        });
+
+        // show the first tab to start off
+        this.tabs[0].isActive = true;
+        this.tabs[0].display(tabContentDiv);
+
+        this.updateTabButtonStyles(tabContainer);
+    }
+
+    /**
+     * Updates the styles for the tab buttons.
+     */
+    private updateTabButtonStyles(tabContainer: HTMLElement): void {
+        const tabButtons = tabContainer.querySelectorAll(".prettier-settings-tab-button");
+
+        tabButtons.forEach((button, index) => {
+            const tab = this.tabs[index];
+            if (tab && tab.isActive) {
+                button.addClass("prettier-settings-tab-button-active");
+            } else {
+                button.removeClass("prettier-settings-tab-button-active");
+            }
+        });
     }
 }
