9b9cc1453347ec2320157598a384b26f494f849c
jheddings <jason@heddings.com>
2025-08-20 21:32:10 -0600

    Switch to obskit.

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,10 @@
 // settings.ts - settings UI for the Obsidian Prettier plugin
 
-import { PluginSettingTab, App, Setting } from "obsidian";
+import { PluginSettingTab, App } from "obsidian";
 import { LogLevel } from "./logger";
 import { ProseWrapOptions } from "./config";
 import PrettierPlugin from "./main";
+import { DropdownSetting, SliderSetting, ToggleSetting } from "obskit";
 
 function generateLinkElement(name: string, docUrl: string): DocumentFragment {
     const fragment = document.createDocumentFragment();
@@ -19,115 +20,6 @@ function generateLinkElement(name: string, docUrl: string): DocumentFragment {
 
 function generatePrettierLink(name: string, option: string): DocumentFragment {
     return generateLinkElement(name, `https://prettier.io/docs/en/options.html#${option}`);
-}
-
-interface SettingConfig {
-    name: string | DocumentFragment;
-    description: string;
-}
-
-/**
- * Base class for reusable setting elements.
- */
-abstract class BaseSetting<T> {
-    protected name: string | DocumentFragment;
-    protected description: string;
-
-    constructor(config: SettingConfig) {
-        this.name = config.name;
-        this.description = config.description;
-    }
-
-    abstract get value(): T;
-
-    abstract set value(val: T);
-
-    abstract get default(): T;
-
-    /**
-     * Creates the setting element in the provided container.
-     */
-    abstract display(containerEl: HTMLElement): Setting;
-}
-
-/**
- * Toggle setting for boolean values.
- */
-abstract class ToggleSetting extends BaseSetting<boolean> {
-    display(containerEl: HTMLElement): Setting {
-        return new Setting(containerEl)
-            .setName(this.name)
-            .setDesc(this.description)
-            .addToggle((toggle) => {
-                toggle.setValue(this.value);
-                toggle.onChange(async (value) => {
-                    this.value = value;
-                });
-            });
-    }
-}
-
-/**
- * Slider setting for numeric values.
- */
-abstract class SliderSetting extends BaseSetting<number> {
-    display(containerEl: HTMLElement): Setting {
-        return new Setting(containerEl)
-            .setName(this.name)
-            .setDesc(this.description)
-            .addSlider((slider) => {
-                slider.setLimits(this.minimum, this.maximum, this.step);
-                slider.setDynamicTooltip();
-                slider.setValue(this.value);
-                slider.onChange(async (value) => {
-                    this.value = value;
-                });
-            });
-    }
-
-    abstract get minimum(): number;
-
-    abstract get maximum(): number;
-
-    abstract get step(): number;
-}
-
-/**
- * Dropdown setting for enumerated values.
- */
-abstract class DropdownSetting<T> extends BaseSetting<T> {
-    display(containerEl: HTMLElement): Setting {
-        return new Setting(containerEl)
-            .setName(this.name)
-            .setDesc(this.description)
-            .addDropdown((dropdown) => {
-                this.options.forEach(({ key, label }) => {
-                    dropdown.addOption(key, label);
-                });
-                dropdown.setValue(this.getKeyForValue(this.value));
-                dropdown.onChange(async (key) => {
-                    this.value = this.getValueForKey(key);
-                });
-            });
-    }
-
-    abstract get options(): { key: string; label: string; value: T }[];
-
-    /**
-     * Get the key for a given value.
-     */
-    protected getKeyForValue(value: T): string {
-        const option = this.options.find((opt) => opt.value === value);
-        return option?.key ?? this.options[0]?.key ?? "";
-    }
-
-    /**
-     * Get the value for a given key.
-     */
-    protected getValueForKey(key: string): T {
-        const option = this.options.find((opt) => opt.key === key);
-        return option?.value ?? this.options[0]?.value;
-    }
 }
 
 /**
